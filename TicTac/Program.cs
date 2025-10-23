73b6bcf7c885fd5c5fdb8db0b289d059523a1f79
Júlia Martins Souza <juliamss07@gmail.com>
2025-10-23 13:45:08 -0300

    Delete TicTac directory - changing some things

--- TicTac/Program.cs
+++
@@ -1,2 +0,0 @@
-﻿using var game = new TicTac.Game1();
-game.Run();
--- TicTac/Scenes/Scene.cs
+++
@@ -1,6 +0,0 @@
-public abstract class Scene
-{
-    public abstract void LoadContent(ContentManager content);
-    public abstract void Update(GameTime gameTime);
-    public abstract void Draw(SpriteBatch spriteBatch);
-}
